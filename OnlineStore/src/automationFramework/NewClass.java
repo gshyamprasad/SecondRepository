9ae168d131e16e8bd3cce418ea58cc7c50ba5b51
gshyamprasad <gshyamprasad@gmail.com>
2019-12-02 23:14:49 +0530

    Added new comment

--- OnlineStore/src/automationFramework/NewClass.java
+++ OnlineStore/src/automationFramework/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 
 		System.out.println("This is New Class........");
+		System.out.println("This is GIT Hub changes......");
 
 	}
 
